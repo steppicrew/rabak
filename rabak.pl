c2f0ef0c0ef264028f76f7788e1015e344cdcb64
steppicrew <steppicrew@fd17d8b8-7a2b-0410-b732-fc0459f805a7>
2007-07-12 15:07:17 +0000

    nomsg
    git-svn-id: https://rabak.svn.sourceforge.net/svnroot/rabak@132 fd17d8b8-7a2b-0410-b732-fc0459f805a7

--- rabak.pl
+++ rabak.pl
@@ -3,4 +3,8 @@
 # wrapper for backward compatibility
 scriptname="`basename "$0" ".pl"`"
 dirname="`dirname "$0"`"
+
+echo "WARNING: please use '$dirname/$scriptname'!"
+echo "calling $0 is depricated!"
+
 "$dirname/$scriptname" "$@"
