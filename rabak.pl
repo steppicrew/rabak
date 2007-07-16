3247cc6b0f56c6fd7b8e2fe159b36edebc30fe40
steppicrew <steppicrew@fd17d8b8-7a2b-0410-b732-fc0459f805a7>
2007-07-16 09:00:37 +0000

    - fixed spelling
    git-svn-id: https://rabak.svn.sourceforge.net/svnroot/rabak@139 fd17d8b8-7a2b-0410-b732-fc0459f805a7

--- rabak.pl
+++ rabak.pl
@@ -5,6 +5,6 @@ scriptname="`basename "$0" ".pl"`"
 dirname="`dirname "$0"`"
 
 echo "WARNING: please use '$dirname/$scriptname'!"
-echo "calling $0 is depricated!"
+echo "calling $0 is deprecated!"
 
 "$dirname/$scriptname" "$@"
