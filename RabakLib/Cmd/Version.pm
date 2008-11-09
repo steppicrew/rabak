e17211c713475f8fd3cd34354054ed578b4b4ae4
steppicrew <rabak@steppicrew.de>
2008-11-09 18:48:26 +0100

    commited Version command

---
+++ RabakLib/Cmd/Version.pm
@@ -0,0 +1,35 @@
+#!/usr/bin/perl;
+
+package RabakLib::Cmd::Version;
+
+use warnings;
+use strict;
+use vars qw(@ISA);
+
+use Data::Dumper;
+use RabakLib::Version;
+
+@ISA= qw( RabakLib::Cmd );
+
+sub getOptions {
+    return {};
+}
+
+sub Help {
+    my $self= shift;
+    return $self->SUPER::Help(
+        'rabak version',
+        'Shows rabak\'s version and copyright notes.',
+    );
+}
+
+sub run {
+    my $self= shift;
+
+    return unless $self->wantArgs(0);
+
+    print RabakLib::Version::LongVersionMsg(), $/;
+    return 1;
+}
+
+1;
