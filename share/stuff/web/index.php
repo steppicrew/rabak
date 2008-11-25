4e49754e0abb3a68e3fcdf3382a7a169e39d0ba4
steppicrew <rabak@steppicrew.de>
2008-11-25 10:31:11 +0100

    css: remove backup-comments

--- share/stuff/web/index.php
+++ share/stuff/web/index.php
@@ -567,7 +567,6 @@
         <meta name="keywords" content="<?= $sPageKeywords ?>">
         <meta name="description" content="<?= $sPageDescription ?>">
         <link rel="stylesheet" type="text/css" href="http://github.com/steppicrew/rabak/tree/master%2Fshare%2Fstuff%2Fweb%2Fscreen.css?raw=true">
-        <!--link rel="stylesheet" type="text/css" href="<?= $sRelPath ?>screen.css"-->
     </head>
     <body class="default">
         <div id="container">
