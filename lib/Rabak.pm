0e0d6bf6714b84f82b047309ac5d77e20c0ea052
dr-berlin <info1@raisin.de>
2009-03-12 02:16:58 +0100

    Testing...

--- lib/Rabak.pm
+++ lib/Rabak.pm
@@ -12,14 +12,31 @@ use Rabak::Log;
 # use Rabak::Peer::Target;
 # use Rabak::Version;
 
-# use Data::Dumper;
+use Data::Dumper;
 # use File::Spec ();
 # use POSIX qw(strftime);
 
-sub API {
-    my %params= @_;
+sub do_test {
+    print Dumper(@_);
+    
+}
 
-    return ();
+sub API {
+    my $cmd= shift;
+    my %params= @_;
+    
+
+    $cmd= "do_$cmd";
+
+    my %result;
+    eval {
+        no strict refs;
+        # %result= 
+        &$cmd(@_);
+    };
+    print $@;
+
+    return %result;
 }
 
 1;
