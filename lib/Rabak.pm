60a23b33aa6685b3fc75abf24c3f2edf6d5ac408
dr-berlin <info1@raisin.de>
2009-03-12 01:46:06 +0100

    Added Rabak.pm stub.

---
+++ lib/Rabak.pm
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+package Rabak;
+
+use warnings;
+use strict;
+
+# no warnings 'redefine';
+
+use Rabak::Log;
+# use Rabak::Peer::Source;
+# use Rabak::Peer::Target;
+# use Rabak::Version;
+
+# use Data::Dumper;
+# use File::Spec ();
+# use POSIX qw(strftime);
+
+sub API {
+    my %params= @_;
+
+    return ();
+}
+
+1;
